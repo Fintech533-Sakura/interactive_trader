e961b8d9198afddd2f212f400700c69aaba7916d
Yudi Zhang <zhan5155@umn.edu>
2022-04-30 18:18:40 -0400

    app UI

--- page_1.py
+++ page_1.py
@@ -1,3 +1,16 @@
-from dash import html
+import dash_bootstrap_components as dbc
+from dash import dcc, html
 
-page_1 = html.P("This is the content of page 1!")
+page_1 = html.Div(
+    [
+        html.P("Pair Trading Strategy"),
+        html.Hr(),
+        html.P("Parameters:"),
+        dbc.Label('Threshold'),
+        dbc.Input(id="threshold", type="number", value=1.5),
+        dbc.Label('Hedge Ratio'),
+        dbc.Input(id="hedge_ratio", type="number", value=1),
+        dbc.Label('Alpha'),
+        dbc.Input(id="alpha", type="number", value=1),
+    ]
+)
