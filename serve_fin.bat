dabd065ee7c1c353f30de28a2875f7caf42de1f2
yy319 <yy319@duke.edu>
2022-04-30 20:15:39 -0400

    add serve_fin.bat

---
+++ serve_fin.bat
@@ -0,0 +1,4 @@
+cd C:\Users\yy319\PycharmProjects\interactive_trader
+git pull https://%TESTAPP_GIT_PAT%@github.com/hushusky/interactive_trader.git
+venv\Scripts\python.exe -m pip install -r requirements.txt
+venv\Scripts\python.exe server.py
